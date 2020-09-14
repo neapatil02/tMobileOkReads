ac0bfcbb4dd0c079376d5d3004a0d56ea56e8111
Patil, Neha Shekhar <nepatil@iu.edu>
2020-09-14 10:32:35 -0400

    dialog

--- libs/books/feature/src/lib/reading-list/reading-list.component.ts
+++ libs/books/feature/src/lib/reading-list/reading-list.component.ts
@@ -1,17 +1,20 @@
-import { Component } from '@angular/core';
+import { Component,TemplateRef,ViewChild } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { getReadingList, markAsReadInReadingList, removeFromReadingList } from '@tmo/books/data-access';
 import { ReadingListItem } from '@tmo/shared/models';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'tmo-reading-list',
   templateUrl: './reading-list.component.html',
   styleUrls: ['./reading-list.component.scss']
 })
+
 export class ReadingListComponent {
   readingList$ = this.store.select(getReadingList);
-
-  constructor(private readonly store: Store) {}
+  @ViewChild('callAPIDialog') callAPIDialog: TemplateRef<any>;
+  constructor(private readonly store: Store,
+    private dialog: MatDialog) {}
 
   removeFromReadingList(item) {
     this.store.dispatch(removeFromReadingList({ item }));
@@ -19,6 +22,7 @@ export class ReadingListComponent {
 
   markAsRead(data: ReadingListItem)
   {
+    let dialogRef = this.dialog.open(this.callAPIDialog);
     this.store.dispatch(markAsReadInReadingList({ item: { id: data.bookId, changes: {
       finished: true,
       finishedDate: new Date().toDateString()
